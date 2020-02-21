e332318d31962bcff8f02948eb7bf477c5f43e3c
Lukman <52183639+Balunywa@users.noreply.github.com>
2020-02-20 22:03:01 -0500

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,7 +3,7 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered rewally!");
+            System.out.println("I am covered really lets see how this will GO!!");
             return;
         }
 
